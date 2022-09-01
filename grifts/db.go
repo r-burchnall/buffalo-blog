2e782f33104db8f6d5bf5377a75ee86235a8f401
Ross Burchnall <ross.burchnall@and.digital>
2022-09-01 14:41:43 +0100

    Added seed for user in db

--- grifts/db.go
+++ grifts/db.go
@@ -2,6 +2,8 @@ package grifts
 
 import (
 	"github.com/gobuffalo/grift/grift"
+	"log"
+	"social_api/models"
 )
 
 var _ = grift.Namespace("db", func() {
@@ -9,6 +11,16 @@ var _ = grift.Namespace("db", func() {
 	grift.Desc("seed", "Seeds a database")
 	grift.Add("seed", func(c *grift.Context) error {
 		// Add DB seeding stuff here
+		defaultUser := &models.User{
+			Email:    "test@test.com",
+			Password: "test",
+		}
+
+		err := models.DB.Create(defaultUser)
+		if err != nil {
+			log.Fatalln("Failed to create user in database")
+		}
+
 		return nil
 	})
 
